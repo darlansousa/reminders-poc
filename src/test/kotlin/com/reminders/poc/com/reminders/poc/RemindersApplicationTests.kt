52c04fd7de2a5b3ad0890cff95daf01d42670629
Darlan Souza <darlansousa.ds@gmail.com>
2023-09-16 15:35:06 -0300

    initial commit

---
+++ src/test/kotlin/com/reminders/poc/com/reminders/poc/RemindersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.reminders.poc.com.reminders.poc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RemindersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
